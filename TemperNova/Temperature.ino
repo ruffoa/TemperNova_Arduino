06fc1ca9d3158a69ee7ef97f1c222c1909092577
ruffoa <ruffo.alex@gmail.com>
2020-02-10 22:36:03 -0500

    Add in code to make Austin happy

--- TemperNova/Temperature.ino
+++ TemperNova/Temperature.ino
@@ -1,6 +1,10 @@
 #define ONE_WIRE_BUS 26 // plugged into IO 26 - https://lh3.googleusercontent.com/masktxO3sTpWCfj9ZQ6AHwEMPhXcx7WANLA63M-qYiTZEPTFEk9DF9K3AzHy0Ct8meEjXf3tZRvrLleTd1yB7FUrbIVHUr_tclueBALq_lZCm-X1hLJjTorYjqZYZkAA_BvucyH0SFHNf97xfxz8Cy7Gqc938HxB6xBof48jTu4l2oRNMuT9PhUWLo04FRA6sb7VNJlc-NNwVQpcfeEJaqaKXL87BXz3Ir73aO776YeNlFlqrdksBnQFweJN4BZTNqZD1kRika4iWz-ZAzIkBF_rBqZ6m3bgSzeL5ZqbYbBWBYkXn9UV2skoV6qk4xF7Zj8DnV96rCIsMG4W4cTo5XGEJEibo6ty7yeuVss0GvJ0tHE0-E7wGtzA1sMIIgS-9A7NvOPfX-iZdqPjIe6wVb6sXdAbkQgu3t0uY5XdR6r8e7wSROKbtQbN2TBrQnIlXzS8IvyCEMvweZU-p1geAANlX91tuvfnmIFjXEfhcLCctHzZSlRQI80J5cJaV-y08Rs6AKLWDhHi7dVjf38SCSlKrMjQ9cwKMLJNCB577ftwh1CVauK4BrbJOBLtGEv00e0VRwaCZ7GQN9szM2gQ0xqG7Apm2pcoiVb1uK3i40SZ7LDLbbaLadDf1gIyexdZvnxT_iYYeB7d5YwnzT7euFqleCs-I0900cB2bktALSlEOCLwSCGIOc4=w984-h1312-no
-#define TECS_PIN 35
-#define TEMP_CONTROL_PIN 33
+#define TECS_ONE_PIN 35
+#define TECS_TWO_PIN 18
+
+#define TEMP_HOT_PIN 33
+#define TEMP_COLD_PIN 19
+
 #define SWAP_COUNT 500
 
 OneWire oneWire(ONE_WIRE_BUS);
@@ -11,8 +15,7 @@ int  resolution = 10;
 unsigned long lastTempRequest = 0;
 int  delayInMillis = 0;
 float temperature = 0.0;
-bool lastOn = false;
-bool wasCold = false;
+bool tecOneOn = false;
 int count = 520;
 
 char *addr2str(DeviceAddress deviceAddress) // translates the device address to a string, used for debugging
@@ -32,8 +35,10 @@ char *addr2str(DeviceAddress deviceAddress) // translates the device address to 
 }
 
 void setupTecs() {
-  pinMode(TECS_PIN, OUTPUT); 
-  pinMode(TECS_PIN, OUTPUT); 
+  pinMode(TECS_ONE_PIN, OUTPUT); 
+  pinMode(TECS_TWO_PIN, OUTPUT); 
+  pinMode(TEMP_HOT_PIN, OUTPUT); 
+  pinMode(TEMP_COLD_PIN, OUTPUT);
 }
 
 void setupTempSensor() {
@@ -94,38 +99,36 @@ void controlTecs() {
   if (count % 2 == 0) {
     if (rTemp > target) {
       // send cooldown command
-      digitalWrite(TEMP_CONTROL_PIN, LOW);
+      digitalWrite(TEMP_COLD_PIN, HIGH);
+      digitalWrite(TEMP_HOT_PIN, LOW);
       Serial.print("Sending cooldown command to the TECS ");
     } else if (rTemp < target) {
       // send heat command
-      digitalWrite(TEMP_CONTROL_PIN, HIGH);
+      digitalWrite(TEMP_COLD_PIN, LOW);
+      digitalWrite(TEMP_HOT_PIN, HIGH);
       Serial.print("Sending heat-up command to the TECS ");
     } else {
-      if (wasCold) {
-        digitalWrite(TEMP_CONTROL_PIN, HIGH);
-        Serial.print("Sending heating command to the TECS (it's currently at temp though) ");
-      } else {
-        digitalWrite(TEMP_CONTROL_PIN, LOW);
-        Serial.print("Sending cooldown command to the TECS (it's currently at temp though) ");
-      }
+        digitalWrite(TEMP_COLD_PIN, LOW);
+        digitalWrite(TEMP_HOT_PIN, LOW);
+        Serial.print("Sending command to turn off the TECS (it's currently at temp) ");
     }
   
-    if (count <= 0) {
-      wasCold = !wasCold;
-      
+    if (count <= 0) {      
       count = SWAP_COUNT;
       
-      lastOn = !lastOn;
-      Serial.println("Hit the 500 cycles, swapping which TEC, and heat / cool state for neutral");
+      tecOneOn = !tecOneOn;
+      Serial.println("Hit the 500 cycles, swapping which TEC is enabled");
     } 
     
     // turn on and off to keep it at temp
-    if (lastOn) {
-        digitalWrite(TECS_PIN, LOW);
-        Serial.println("Using TEC 1 (PIN LOW)");
+    if (tecOneOn) {
+        digitalWrite(TECS_ONE_PIN, HIGH);
+        digitalWrite(TECS_TWO_PIN, LOW);
+        Serial.println("Using TEC 1 (TEC 2 Off)");
     } else {
-        digitalWrite(TECS_PIN, HIGH);
-        Serial.println("Using TEC 2 (PIN HIGH)");
+        digitalWrite(TECS_TWO_PIN, HIGH);
+        digitalWrite(TECS_ONE_PIN, LOW);
+        Serial.println("Using TEC 2 (TEC 1 Off)");
     }
   }
   
